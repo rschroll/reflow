1938c091ab9f616fb4e192485691625f59e892c9
Robert Schroll <rschroll@gmail.com>
2014-01-31 02:46:00 -0500

    Make width into a setting

--- src/lt/plugins/parwrap.cljs
+++ src/lt/plugins/parwrap.cljs
@@ -6,19 +6,19 @@
             [lt.objs.files :as files])
   (:require-macros [lt.macros :refer [behavior defui]]))
 
-(defn rewrap [text]
+(defn rewrap [text width]
   (let [text (.replace text (js/RegExp. "\\s*\n" "g") " ")]
-    (_rewrap text)))
+    (_rewrap text width)))
 
-(defn _rewrap [text]
-  (if (>= 80 (.-length text))
+(defn _rewrap [text width]
+  (if (>= width (.-length text))
     text
-    (let [re (js/RegExp. "^((.{0,79}\\S)\\s+(.*)|(\\S*)\\s*(.*))")
+    (let [re (js/RegExp. (str "^((.{0," (- width 1) "}\\S)\\s+(.*)|(\\S*)\\s*(.*))"))
           match (.exec re text)
           line (or (aget match 2) (aget match 4))
           more (or (aget match 3) (aget match 5))]
       (if (< 0 (.-length more))
-        (str line "\n" (_rewrap more))
+        (str line "\n" (_rewrap more width))
         line))))
 
 (behavior ::rewrap
@@ -35,7 +35,17 @@
                           (if (<= from to)
                             (ed/set-selection editor {:line from :ch 0} {:line to}))))
                       (if (ed/selection? editor)
-                        (ed/replace-selection editor (rewrap (ed/selection editor))))))
+                        (let [width (ed/option editor :parwrap-width)]
+                          (ed/replace-selection editor (rewrap (ed/selection editor) width))))))
+
+(behavior ::wrap-width
+          :triggers #{:object.instant}
+          :desc "Editor: paragraph wrap width"
+          :params [{:label "Wrap width" :type :number}]
+          :type :user
+          :exclusive true
+          :reaction (fn [editor width]
+                      (ed/set-options editor {:parwrap-width width})))
 
 (cmd/command {:command :parwrap-rewrap
               :desc "Rewrap paragraph"
